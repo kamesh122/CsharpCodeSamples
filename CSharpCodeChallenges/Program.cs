9f93e3221979cd6c6cb10f9d4e00d29f24a59c5d
kamesh Rao <kamesh122@gmail.com>
2024-07-26 16:49:46 -0700

    update

--- CSharpCodeChallenges/Program.cs
+++ CSharpCodeChallenges/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CSharpCodeChallenges.Samples;
+
+string Input = "Kamesh";
+Console.WriteLine("Input : " + Input);
+ReverseInputString reverseInputString = new ReverseInputString();
+var reversestring = reverseInputString.ReverseAllWords(Input.ToCharArray());
+Console.WriteLine("Reverse String output1 : " + string.Join("", reversestring));
+Console.WriteLine("Reverse String output without inbuilt function : " + reverseInputString.ReverseString(Input));
 Console.ReadLine();
---
+++ CSharpCodeChallenges/Samples/ReverseInputString.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCodeChallenges.Samples
+{
+    public class ReverseInputString
+    {
+
+        public char[] ReverseAllWords(char[] in_text)
+        {
+            int lindex = 0;
+            int rindex = in_text.Length - 1;
+            if (rindex > 1)
+            {
+                //reverse complete phrase
+                in_text = ReverseString(in_text, lindex, rindex);
+
+                //    //reverse each word in resultant reversed phrase
+                //    for (rindex = 0; rindex <= in_text.Length; rindex++)
+                //    {
+                //        if (rindex == in_text.Length || in_text[rindex] == ' ')
+                //        {
+                //            in_text = ReverseString(in_text, lindex, rindex - 1);
+                //            lindex = rindex + 1;
+                //        }
+                //    }
+            }
+            return in_text;
+        }
+
+        public char[] ReverseString(char[] intext, int lindex, int rindex)
+        {
+            char tempc;
+            while (lindex < rindex)
+            {
+                tempc = intext[lindex];
+                intext[lindex++] = intext[rindex];
+                intext[rindex--] = tempc;
+            }
+            return intext;
+        }
+
+        public string ReverseString(string myString)
+        {
+            string reversedString = string.Empty;
+            
+            for (int i = myString.Length - 1; i >= 0; i--)
+            {
+                reversedString += myString[i];
+            }
+
+            return reversedString;
+        }
+    }
+}
